9cdd10f4bcc8156f486b210d298fec90c9af9d80
selim <selim-kose@live.com>
2025-05-30 12:41:49 +0200

    Add kafka producer and config

---
+++ app-config-data/src/main/java/se/selimkose/config/KafkaProducerConfigData.java
@@ -0,0 +1,22 @@
+package se.selimkose.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "kafka.producer")
+public class KafkaProducerConfigData {
+
+    private String keySerializer;
+    private String valueSerializer;
+    private String compressionType;
+    private String acks;
+    private Integer batchSize;
+    private Integer batchSizeBoostFactor;
+    private Integer lingerMs;
+    private Integer requestTimeoutMs;
+    private Integer retryCount;
+
+}
